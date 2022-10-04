07e05b043f322c0168ad19648b442defe0365311
Imperial Tu Ho <Waffenss436@gmail.com>
2022-10-04 17:46:31 +0700

    Edit Category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -14,7 +14,8 @@ namespace BulkyBookWeb.Controllers {
             IEnumerable<Category> objectCategoryList = _db.Categories;
             return View(objectCategoryList);
         }
-        
+
+        /*  CREATE FEATURE  */
         //GET
         public IActionResult Create() {
             return View();
@@ -34,5 +35,35 @@ namespace BulkyBookWeb.Controllers {
             }
             return View(obj);
         }
+
+        /*  EDIT FEATURE  */
+        //GET
+        public IActionResult Edit(int? id) {
+            if (id == null || id == 0) {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null) {
+                return NotFound();
+            }
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj) {
+            if (obj.Name == obj.DisplayOrder.ToString()) {
+                ModelState.AddModelError("Name", "The Name cannot exactly match the DisplayOrder.");
+            }
+            if (ModelState.IsValid) {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
     }
 }
