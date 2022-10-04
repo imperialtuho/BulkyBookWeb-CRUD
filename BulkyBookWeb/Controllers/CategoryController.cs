164aef96b350467ab5d383737a2652123a157186
ImperialTuHo <ImperialTuHo@IMPERIALTUHO-PC>
2022-10-04 12:10:14 +0700

    Server Side Validation

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Controllers {
+    
+    public class CategoryController : Controller {
+
+        private readonly ApplicationDbContext _db;
+        
+        public CategoryController(ApplicationDbContext db) { _db = db; }
+        
+        public IActionResult Index() {
+            IEnumerable<Category> objectCategoryList = _db.Categories;
+            return View(objectCategoryList);
+        }
+        
+        //GET
+        public IActionResult Create() {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj) {
+            if (ModelState.IsValid) {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
